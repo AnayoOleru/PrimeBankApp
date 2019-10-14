9c1eb470d44e1510dbb1851f3c2094ca035b517e
Anayo Oleru <anayooleru@Anayos-MacBook-Air.local>
2019-10-14 23:35:38 +0100

    story/signin - fix bug on vector icon - round up signin component

--- screens/SigninScreen.js
+++ screens/SigninScreen.js
@@ -13,7 +13,9 @@ class SignIn extends React.Component {
             firstName: '',
             lastName: '',
             phoneNumber: '',
-            password: ''
+            password: '',
+            showPass: true,
+            press: false
         }
     }
 
@@ -39,21 +41,40 @@ class SignIn extends React.Component {
     render() {
         const { navigate } = this.props.navigation;
         return (
-            <ImageBackground source={bgImage} style={styles.backgroundContainer}>
+            <ImageBackground style={styles.backgroundContainer}>
                 <View style={styles.logoContainer}>
                     <Image source={logo} style={styles.logo} />
-                    <Text style={styles.logoText}>SIGN-IN </Text>
+                    <Text style={styles.logoText}>LOGIN </Text>
                 </View>
-                <View>
-                    <Ionicons name={'ios-person-online'} size={28} color={'rgba(255 255, 255, 0.7)'} 
+                <View style={styles.inputContainer}>
+                    <Ionicons name={'email-lock'} size={25} color={'white'} 
                     style={styles.inputIcon} />
                     <TextInput
                         style={styles.input}
-                        placeholder={'Username'}
-                        placeholderTextColor={'rgba(255 255, 255, 0.7)'}
+                        placeholder={'Email'}
+                        placeholderTextColor={'white'}
                         underlineColorAndroid='transparent'
                     />
                 </View>
+                <View style={styles.inputContainer}>
+                    <Ionicons name={'folder-lock-open'} size={25} color={'white'} 
+                    style={styles.inputIcon} />
+                    <TextInput
+                        style={styles.input}
+                        placeholder={'Password'}
+                        secureTextEntry={this.state.showPass}
+                        placeholderTextColor={'white'}
+                        underlineColorAndroid='transparent'
+                    />
+
+                    <TouchableOpacity style={styles.btnEye }>
+                        <Ionicons name={this.state.press === false ? 'arrow-with-circle-right' : null} size={22} color={'white'} />
+                    </TouchableOpacity>
+                </View>
+
+                <TouchableOpacity style={styles.btnLogin }>
+                        <Text style={styles.text}>Login</Text>
+                    </TouchableOpacity>
             </ImageBackground>
         )
     }
@@ -67,9 +88,11 @@ const styles = {
         alignItems: 'center',
         width: null,
         height: null,
+        backgroundColor: '#062b50'
     },
     logoContainer: {
         alignItems: 'center',
+        marginBottom: 50
     },
     logo: {
         width: 120,
@@ -77,7 +100,7 @@ const styles = {
     },
     logoText: {
         color: 'white',
-        fontSize: 20,
+        fontSize: 25,
         fontWeight: '500',
         marginTop: 10,
         opacity: 0.5
@@ -87,15 +110,37 @@ const styles = {
         height: 45,
         borderRadius: 45,
         fontSize: 16,
-        paddingLeft: 45,
+        paddingLeft: 60,
         backgroundColor: 'rgba(0, 0, 0, 0.35)',
-        color: 'rgba(255 255, 255, 0.7)',
+        color: 'white',
         marginHoriontal: 25
     },
     inputIcon: {
         position: 'absolute',
-        top: 8,
+        top: 10,
         left: 37 
+    },
+    inputContainer: {
+        marginTop: 10
+    },
+    btnEye: {
+        position: 'absolute',
+        top: 10,
+        right: 37  
+    },
+    btnLogin: {
+        width: WIDTH - 55,
+        height: 45,
+        borderRadius: 45,
+        backgroundColor: '#fcbb16',
+        justifyContent: 'center',
+         marginTop: 20
+    },
+    text: {
+        color: 'white',
+         fontSize: 16,
+         textAlign: 'center'
+
     }
 }
 
